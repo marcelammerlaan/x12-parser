511c91b368a1cf259d77c20bdf5961e2f0ea3f54
piaweb2 <piaweb2@VDS00106.VPO024.local>
2017-09-08 15:12:53 +0200

    Strongly typed integer X12 element.

---
+++ src/main/java/com/dyskolus/x12/X12IntegerElement.java
@@ -0,0 +1,30 @@
+package com.dyskolus.x12;
+
+public class X12IntegerElement implements X12Element {
+	private final int content;
+	
+	public X12IntegerElement(int s) {
+		this.content = s;
+	}
+	
+	public String toString() {
+		return ""+content;
+	}
+	
+	@Override
+	public int hashCode() {
+		return content;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof X12IntegerElement) {
+			return ((X12IntegerElement)o).content == this.content;
+		}
+		return false;
+	}
+
+	public int length() {
+		return this.toString().length();
+	}
+}
